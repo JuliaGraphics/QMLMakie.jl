308711774bd31b4f7129523bafb422e20197f5e0
Steffen Haug <stehau@pm.me>
2024-09-07 19:03:59 +0200

    Ripped Makie Support code from QML

---
+++ examples/Lorenz/lorenz.jl
@@ -0,0 +1,104 @@
+ENV["QSG_RENDER_LOOP"] = "basic"
+
+using GLMakie
+set_theme!(theme_black())
+
+using CxxWrap
+using Makie
+using QML
+using QMLMakie
+QML.setGraphicsApi(QML.OpenGL)
+
+
+Base.@kwdef mutable struct Lorenz
+  frame::Int64 = 0
+  dt::Float64 = 0.001
+  σ::Float64 = 10
+  ρ::Float64 = 28
+  β::Float64 = 8 / 3
+  x::Float64 = 1
+  y::Float64 = 1
+  z::Float64 = 1
+end
+
+function step!(l::Lorenz)
+  dx = l.σ * (l.y - l.x)
+  dy = l.x * (l.ρ - l.z) - l.y
+  dz = l.x * l.y - l.β * l.z
+  l.x += l.dt * dx
+  l.y += l.dt * dy
+  l.z += l.dt * dz
+  Point3f(l.x, l.y, l.z)
+end
+
+const QML_SRC = QByteArray("""
+import QtQuick
+import QtQuick.Controls
+import org.julialang
+
+ApplicationWindow {
+  title: "Test"
+  visible: true
+  width: 400
+  height: 400
+
+  MakieViewport {
+    id: lorenz
+    anchors.fill: parent
+    renderFunction: redraw
+  }
+
+  Button {
+    text: tick.running? "Pause" : "Play"
+    anchors.bottom: parent.bottom
+    anchors.horizontalCenter: parent.horizontalCenter
+    onClicked: tick.running = !tick.running
+  }
+
+  Timer {
+    id: tick
+    interval: 8
+    running: true
+    repeat: true
+    onTriggered: {
+      Julia.step()
+      lorenz.update()
+    }
+  }
+}
+""")
+
+const attractor = Lorenz()
+const points = Observable(Point3f[])
+const colors = Observable(Int[])
+
+fig, ax, l = lines(points, color=colors,
+  colormap=:inferno, transparency=true,
+  axis=(; type=Axis3, protrusions=(0, 0, 0, 0),
+    viewmode=:fit, limits=(-30, 30, -30, 30, 0, 50)))
+
+function step!()
+  for i in 1:20
+    push!(points[], step!(attractor))
+    push!(colors[], attractor.frame)
+  end
+  ax.azimuth[] = 1.7pi + 0.3 * sin(2pi * attractor.frame / 1200)
+  notify(points)
+  notify(colors)
+  l.colorrange = (0, attractor.frame)
+  attractor.frame += 1
+end
+
+const QT_ENGINE = init_qmlengine()
+
+redraw(screen) = display(screen, fig.scene)
+
+ctx = root_context(QT_ENGINE)
+set_context_property(ctx, "redraw", @safe_cfunction(redraw, Cvoid, (Any,)))
+QML.qmlfunction("step", step!)
+
+qcomp = QQmlComponent(QT_ENGINE)
+set_data(qcomp, QML_SRC, QML.QUrl())
+create(qcomp, qmlcontext())
+
+exec()
---
+++ src/QMLMakie.jl
@@ -0,0 +1,241 @@
+module QtMakie
+
+import jlqml_jll
+
+using GLMakie
+using CxxWrap
+using QML
+using ModernGL
+using Colors
+using FixedPointNumbers
+using LinearAlgebra
+
+const enable_SSAO = Ref(false)
+const enable_FXAA = Ref(true)
+
+@wrapmodule jlqml_jll.get_libjlqml_path :define_julia_module_makie
+
+function __init__()
+  @initcxx
+end
+
+mutable struct QMLGLContext
+  valid::Bool
+  fbo::CxxPtr{QML.QOpenGLFramebufferObject}
+end
+
+function GLMakie.ShaderAbstractions.native_switch_context!(ctx::QMLGLContext)
+  if !ctx.valid
+    try
+      error("Attempt to bind invalid context $ctx")
+    catch e
+      Base.printstyled("ERROR: "; color=:red, bold=true)
+      Base.showerror(stdout, e)
+      Base.show_backtrace(stdout, Base.catch_backtrace())
+    end
+  end
+  QML.bind(ctx.fbo)
+end
+
+function GLMakie.ShaderAbstractions.native_context_alive(ctx::QMLGLContext)
+  return ctx.valid
+end
+
+const ScreenID = GLMakie.ScreenID
+const ZIndex = GLMakie.ZIndex
+const RenderObject = GLMakie.RenderObject
+const ScreenArea = GLMakie.ScreenArea
+
+@cxxdereference function sizetuple(fbo::QML.QOpenGLFramebufferObject)
+  fbosize = QML.size(fbo)
+  return (Int(QML.width(fbosize)), Int(QML.height(fbosize)))
+end
+
+mutable struct QMLScreen <: GLMakie.GLScreen
+  glscreen::QMLGLContext
+  shader_cache::GLMakie.GLAbstraction.ShaderCache
+  screen2scene::Dict{WeakRef,ScreenID}
+  screens::Vector{ScreenArea}
+  renderlist::Vector{Tuple{ZIndex,ScreenID,RenderObject}}
+  postprocessors::Vector{GLMakie.PostProcessor}
+  cache::Dict{UInt64,RenderObject}
+  cache2plot::Dict{UInt32,AbstractPlot}
+  framecache::Matrix{RGB{N0f8}}
+  framebuffer::GLMakie.GLFramebuffer
+  # render_tick::Observable{Nothing}
+  # window_open::Observable{Bool}
+
+  @cxxdereference function QMLScreen(fbo::QML.QOpenGLFramebufferObject)
+    ctx = QMLGLContext(true, CxxPtr(fbo))
+    GLMakie.ShaderAbstractions.switch_context!(ctx)
+    fbosize = sizetuple(fbo)
+    fb = GLMakie.GLFramebuffer(fbosize)
+    shader_cache = GLMakie.GLAbstraction.ShaderCache()
+    newscreen = new(
+      ctx,
+      shader_cache,
+      Dict{WeakRef,ScreenID}(),
+      ScreenArea[],
+      Tuple{ZIndex,ScreenID,RenderObject}[],
+      [
+        enable_SSAO[] ? GLMakie.ssao_postprocessor(fb, shader_cache) : GLMakie.empty_postprocessor(),
+        GLMakie.OIT_postprocessor(fb, shader_cache),
+        enable_FXAA[] ? GLMakie.fxaa_postprocessor(fb, shader_cache) : GLMakie.empty_postprocessor(),
+        to_qmlscreen_postprocessor(fb, shader_cache)
+      ],
+      Dict{UInt64,RenderObject}(),
+      Dict{UInt32,AbstractPlot}(),
+      Matrix{RGB{N0f8}}(undef, fbosize),
+      fb,
+      # Observable(nothing),
+      # Observable(true),
+    )
+    finalizer(newscreen) do s
+      empty!.((s.renderlist, s.screens, s.cache, s.screen2scene, s.cache2plot, s.postprocessors))
+      return
+    end
+  end
+end
+
+function update_fbo!(screen, fbo)
+  screen.glscreen.fbo = CxxPtr(fbo)
+  GLMakie.ShaderAbstractions.switch_context!(screen.glscreen)
+  fbosize = sizetuple(fbo)
+  resize!(screen.framebuffer, fbosize)
+  screen.framecache = Matrix{RGB{N0f8}}(undef, fbosize)
+  return screen
+end
+
+Base.isopen(screen::QMLScreen) = true
+GLMakie.GeometryBasics.widths(screen::QMLScreen) = sizetuple(screen.glscreen.fbo)
+
+# From rendering.jl in GLMakie, with only slight adaptations
+function GLMakie.render_frame(screen::QMLScreen; resize_buffers=true)
+  w, h = sizetuple(screen.glscreen.fbo)
+  fb = screen.framebuffer
+  if resize_buffers
+    resize!(fb, (w, h))
+  end
+
+  # prepare stencil (for sub-scenes)
+  glEnable(GL_STENCIL_TEST)
+  glBindFramebuffer(GL_FRAMEBUFFER, fb.id[1]) # color framebuffer
+  glDrawBuffers(length(fb.render_buffer_ids), fb.render_buffer_ids)
+  glEnable(GL_STENCIL_TEST)
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE)
+  glStencilMask(0xff)
+  glClearStencil(0)
+  glClearColor(0, 0, 0, 0)
+  glClear(GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT | GL_COLOR_BUFFER_BIT)
+  glDrawBuffer(fb.render_buffer_ids[1])
+  GLMakie.setup!(screen)
+  glDrawBuffers(length(fb.render_buffer_ids), fb.render_buffer_ids)
+
+  # render with FXAA & SSAO
+  glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE)
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE)
+  glStencilMask(0x00)
+  GLMakie.GLAbstraction.render(screen) do robj
+    return !Bool(robj[:transparency][]) && Bool(robj[:ssao][])
+  end
+  # SSAO
+  screen.postprocessors[1].render(screen)
+
+  # render with FXAA but no SSAO
+  glDrawBuffers(2, [GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1])
+  glEnable(GL_STENCIL_TEST)
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE)
+  glStencilMask(0x00)
+  # render all non ssao
+  GLMakie.GLAbstraction.render(screen) do robj
+    return !Bool(robj[:transparency][]) && !Bool(robj[:ssao][])
+  end
+  glDisable(GL_STENCIL_TEST)
+
+  # TRANSPARENT RENDER
+  # clear sums to 0
+  glDrawBuffer(GL_COLOR_ATTACHMENT2)
+  glClearColor(0, 0, 0, 0)
+  glClear(GL_COLOR_BUFFER_BIT)
+  # clear alpha product to 1
+  glDrawBuffer(GL_COLOR_ATTACHMENT3)
+  glClearColor(1, 1, 1, 1)
+  glClear(GL_COLOR_BUFFER_BIT)
+  # draw
+  glDrawBuffers(3, [GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT3])
+  glEnable(GL_STENCIL_TEST)
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE)
+  glStencilMask(0x00)
+  # Render only transparent objects
+  GLMakie.GLAbstraction.render(screen) do robj
+    return Bool(robj[:transparency][])
+  end
+  glDisable(GL_STENCIL_TEST)
+
+  # TRANSPARENT BLEND
+  screen.postprocessors[2].render(screen)
+
+  # FXAA
+  screen.postprocessors[3].render(screen)
+
+  # transfer everything to the screen
+  screen.postprocessors[4].render(screen)
+end
+
+# Slightly adapted from GLMakie/postprocessing.jl/to_screen_postprocessor
+function to_qmlscreen_postprocessor(framebuffer, shader_cache)
+  # draw color buffer
+  shader = GLMakie.GLAbstraction.LazyShader(
+    shader_cache,
+    GLMakie.loadshader("postprocessing/fullscreen.vert"),
+    GLMakie.loadshader("postprocessing/copy.frag")
+  )
+  data = Dict{Symbol,Any}(
+    :color_texture => framebuffer.buffers[:color]
+  )
+  pass = RenderObject(data, shader, GLMakie.PostprocessPrerender(), nothing)
+  pass.postrenderfunction = () -> GLMakie.draw_fullscreen(pass.vertexarray.id)
+
+  full_render = screen -> begin
+    fb = screen.framebuffer
+    w, h = sizetuple(screen.glscreen.fbo)
+    # w, h = size(fb) # original from Makie
+
+    QML.bind(screen.glscreen.fbo) # Transfer everything to the QMLScreen
+    glViewport(0, 0, w, h)
+    glClear(GL_COLOR_BUFFER_BIT)
+    GLMakie.GLAbstraction.render(pass) # copy postprocess
+  end
+  return GLMakie.PostProcessor(GLMakie.GLAbstraction.RenderObject[pass], full_render)
+end
+
+function Base.empty!(screen::QMLScreen)
+  empty!(screen.renderlist)
+  empty!(screen.screen2scene)
+  empty!(screen.screens)
+end
+
+function Base.display(screen::QMLScreen, scene::Scene)
+  scene.events.window_area[] = Makie.IRect(0, 0, sizetuple(screen.glscreen.fbo)...)
+  empty!(screen)
+  insertplots!(screen, scene)
+  # Makie.update!(scene)
+  GLMakie.render_frame(screen)
+  return
+end
+
+function setup_screen(fbo)
+  return QMLScreen(fbo)
+end
+
+function setup_screen(screen, fbo)
+  update_fbo!(screen, fbo)
+  return screen
+end
+
+function on_context_destroy()
+  GLMakie.ShaderAbstractions.switch_context!()
+  return
+end
+
+end # module QtMakie
